f4c98b900670a3756397ee0420910af062c7c869
aditya2in <aditya2in@gmail.com>
2026-08-17 12:53:46 +0530

    style: disable sticky notes from autostart

--- hyprland All/.config/hypr/autostart.lua
+++ hyprland All/.config/hypr/autostart.lua
@@ -1,7 +1,7 @@
 -- Extra autostart processes.
 o.exec_on_start("sleep 10 && uwsm-app -- /home/adityaws/DOTfiles/scripts/hyprsunset_watch.sh")
 o.launch_on_start("brave http://localhost:8384")
-o.launch_on_start("sticky")
+-- o.launch_on_start("sticky")
 
 -- Command Center Startup
 o.launch_on_start("ghostty")
